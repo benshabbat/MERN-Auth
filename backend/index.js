f1b9e039af41c41216db5861b04927309a2773e0
benshabbat <benshabbat27@gmail.com>
2024-08-15 11:15:09 +0300

    working the connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
 
+dotenv.config();
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Hello")
-})
-app.listen(3000,()=>{
-    console.log("Server listening on port 3000");
-})
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+app.listen(3000, () => {
+  connectDB();
+  console.log("Server listening on port 3000");
+});
