9d1b0ca3f190cdd1f7e7f00a1c239ef784de3dff
benshabbat <benshabbat27@gmail.com>
2024-08-16 17:53:07 +0300

    basic of auth

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js"
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+app.use("/api/auth",authRoutes)
+
 app.listen(3000, () => {
   connectDB();
   console.log("Server listening on port 3000");
