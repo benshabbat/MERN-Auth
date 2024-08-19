bf0d2ffdd184c5efaba950f6e16529a810afcd48
benshabbat <benshabbat27@gmail.com>
2024-08-19 18:38:51 +0300

    userAlreadyExists

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,16 @@
 export const signup = async (req, res) => {
-  res.send("sign up");
+  const {name, email, password}= req.body;
+  try {
+    if(!name||!email||!password){throw new Error("all fields are required");}
+
+    const userAlreadyExists = await User.findOne({email})
+    if(userAlreadyExists)
+    {
+        res.status(400).json({message: "User already exists"})
+    }
+  } catch (error) {
+    
+  }
 };
 export const login = async (req, res) => {
   res.send("login");
--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,15 @@ import authRoutes from "./routes/auth.route.js"
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
-
+app.use(express.json());
 app.use("/api/auth",authRoutes)
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server listening on port 3000");
+  console.log("Server listening on port: ",PORT);
 });
