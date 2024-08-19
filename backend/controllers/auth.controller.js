fce7fc2cabcc6922db93ac6ce2e8d254b6d1f20c
benshabbat <benshabbat27@gmail.com>
2024-08-19 21:13:32 +0300

    hashedPassword

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,15 +1,23 @@
-export const signup = async (req, res) => {
-  const {name, email, password}= req.body;
-  try {
-    if(!name||!email||!password){throw new Error("all fields are required");}
+import bcryptjs from "bcryptjs";
+import { User } from "../models/user.model";
 
-    const userAlreadyExists = await User.findOne({email})
-    if(userAlreadyExists)
-    {
-        return res.status(400).json({success:false,message: "User already exists"})
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    if (!name || !email || !password) {
+      throw new Error("all fields are required");
     }
+
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
   } catch (error) {
-    res.status(400).json({success:false,message: error.message});
+    res.status(400).json({ success: false, message: error.message });
   }
 };
 export const login = async (req, res) => {
